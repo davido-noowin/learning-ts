efe683c43e94dcad894c01a1377feb65ba32d801
David Nguyen <davido.nguyen1212@gmail.com>
2025-01-31 16:54:56 -0800

    basic datatype coverage

--- datatypes.js
+++ datatypes.js
@@ -1,69 +1,36 @@
+"use strict";
 // `npx tsc` creates a js file based on the ts file (compile the typescript file)
-
 // first_name = 10; -> unable to change type of first_name into int
-
-
 /* STRINGS */
 // type inference
 let first_name = 'John';
-
 // type declaration
-let lname : string = 'Smith';
+let lname = 'Smith';
 let new_name = lname.toLowerCase();
 console.log(new_name);
-
-
 /* NUMBERS */
-let age : number;
-
+let age;
 age = 25;
 age = 25.5;
-
-let dob : string = "25";
-
+let dob = "25";
 let result = parseInt(dob);
-
-
 /* BOOLEANS */
 // let is_valid : boolean -> boolean declarations are undefined
-let is_valid : boolean = true;
-
-
+let is_valid = true;
 /* ARRAYS */
-let string_list : string[];
-let another_string_list : Array<string>;
-
-let number_list : Array<number>;
+let string_list;
+let another_string_list;
+let number_list;
 number_list = [1, 2, 3, 4, 5];
 number_list.push(10);
 // number_list.push("y"); -> only numbers are allowed to be appended
-let filtered_result = number_list.filter((num)=> num > 2);
+let filtered_result = number_list.filter((num) => num > 2);
 console.log(filtered_result);
-
-
-/* ENUM */
-const enum Color {
-    Red,
-    Green,
-    Blue,
-}
-
-let c : Color = Color.Blue;
-
-
+let c = 2 /* Color.Blue */;
 /* TUPLE */
-let swap_numbers : [number, number];
-
-function swap(num1 : number, num2 : number) : [number, number] {
+let swap_numbers;
+function swap(num1, num2) {
     return [num2, num1];
 }
-
 swap_numbers = swap(10, 20);
 console.log(swap_numbers);
-
-
-/* ANY */
-// no exceptions thrown, it accepts any datatypes
-let department : any;
-department = "Number1";
-department = 10;
